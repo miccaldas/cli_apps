7c71c1f7d1347cb2207931820c2c22028f9d599f
mclds <mclds@protonmail.com>
2022-02-24 05:15:08 +0000

    24-02-2022 commit

--- cli_apps/yay_querying/celery/celeryconfig.py
+++ cli_apps/yay_querying/celery/celeryconfig.py
@@ -3,15 +3,14 @@ import redbeat
 from app import app
 from celery import Celery
 from celery.schedules import crontab
-from readbeat import RedBeatSchedulerEntry, interval
+from readbeat import RedBeatSchedulerEntry
 
 result_backend = "redis://localhost:6379/0"
 broker_url = "redis://localhost:6379/0"
 app.autodiscover_tasks(packages=["celery"])
 timezone = "Europe/Lisbon"
-redbeat_redis_url = "redis://localhost:6379/0"
-interval = crontab(day_of_week=3)
-entry = RedBeatSchedulerEntry("yay_cron", "tasks.run", interval, app="celery")
+redbeat_redis_url = "redis://localhost:6379/1"
+entry = RedBeatSchedulerEntry("yay_cron", "tasks.run", crontab(day_of_week), app="celery")
 entry.save
 """beat_schedule = {
     "yay_cron": {
