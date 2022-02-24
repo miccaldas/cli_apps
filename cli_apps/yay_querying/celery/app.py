812c195b41a50fcae89f33df0c63c2fa0d6888a8
mclds <mclds@protonmail.com>
2022-02-24 03:54:20 +0000

    24-02-2022 commit

--- cli_apps/yay_querying/celery/app.py
+++ cli_apps/yay_querying/celery/app.py
@@ -4,7 +4,8 @@ backend for worker information, database,
 and tells it to include the main.py file
 as the module to launch, when called.
 """
-from celery import Celery, crontab
+from celery import Celery
+from celery.schedules import crontab
 
 app = Celery("celery", backend="redis://localhost:6379/0", broker="redis://localhost:6379/0", include=["main"], redbeat_redis_url="redis://localhost:6379/0")
 
