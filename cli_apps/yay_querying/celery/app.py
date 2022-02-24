e3172a19ddd4011213a6dc05332a979b88299dfe
mclds <mclds@protonmail.com>
2022-02-24 03:46:18 +0000

    24-02-2022 commit

--- cli_apps/yay_querying/celery/app.py
+++ cli_apps/yay_querying/celery/app.py
@@ -6,12 +6,7 @@ as the module to launch, when called.
 """
 from celery import Celery, crontab
 
-app = Celery(
-    "celery",
-    backend="redis://localhost:6379/0",
-    broker="redis://localhost:6379/0",
-    include=["main"],
-)
+app = Celery("celery", backend="redis://localhost:6379/0", broker="redis://localhost:6379/0", include=["main"], redbeat_redis_url="redis://localhost:6379/0")
 
 if __name__ == "__main__":
     app.start()
