2a3692e48c4a908647c99475a5ed323ad229381d
micaldas <micaldas@mailfence.com>
2023-07-22 14:11:14 +0100

    22-07-2023 commit

--- cli_apps/pip_data/pip_project/pip_project/settings.py
+++ cli_apps/pip_data/pip_project/pip_project/settings.py
@@ -95,3 +95,5 @@ FEEDS = {'results.csv': {'format': 'csv', 'fields': ['name', 'content'],},}
 RETRY_TIMES = 1
 FEEDS = {'results.csv': {'format': 'csv', 'fields': ['name', 'content'],},}
 RETRY_TIMES = 1
+FEEDS = {'results.csv': {'format': 'csv', 'fields': ['name', 'content'],},}
+RETRY_TIMES = 1
